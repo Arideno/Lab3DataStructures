5246444f3c0787baf66b372e7a47edad88b3f54d
Dasha <grimalskayad@gmail.com>
2020-03-18 20:53:07 +0200

    Add Heap Class

---
+++ Heap.cpp
@@ -0,0 +1,5 @@
+//
+// Created by daria on 18.03.20.
+//
+
+#include "Heap.h"
---
+++ Heap.h
@@ -0,0 +1,25 @@
+//
+// Created by daria on 18.03.20.
+//
+
+#include <vector>
+
+using namespace std;
+
+#ifndef LAB3DATASTRUCTURES_HEAP_H
+#define LAB3DATASTRUCTURES_HEAP_H
+
+
+class Heap {
+private:
+    vector<int> arr;
+public:
+    void add(int n);
+    void delMin();
+    int getMin();
+    bool isEmpty();
+    void heapify(int i);
+};
+
+
+#endif //LAB3DATASTRUCTURES_HEAP_H
