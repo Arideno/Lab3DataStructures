ea6875521075703e9bc5d90129e432a72dec1cf3
Andrii Moisol <andrii.moisol@gmail.com>
2020-03-19 12:52:00 +0200

    Add closed set and open set

--- main.cpp
+++ main.cpp
@@ -2,11 +2,19 @@
 #include "Heap.h"
 #include <fstream>
 #include "Node.h"
+#include <cmath>
+#include <set>
 
 using namespace std;
 
 ifstream in;
 ofstream out;
+
+double distance(Node* node1, Node* node2) {
+    double dx = abs(node1->x - node2->y);
+    double dy = abs(node1->y - node2->y);
+    return dx + dy;
+}
 
 int main() {
     in.open("../maze.txt");
@@ -26,10 +34,24 @@ int main() {
     Node* start = grid[x][y];
     cin >> x >> y;
     Node* end = grid[x][y];
+    start->g = 0;
+    start->h = distance(start, end);
+    start->f = start->g + start->h;
 
+    set<Node*> closedSet;
+    Heap* openSet = new Heap();
 
+    openSet->add(start);
 
-
+    while (!openSet->isEmpty()) {
+        Node* current = openSet->getMin();
+        openSet->delMin();
+        closedSet.insert(current);
+        if (current == end) {
+            cout << "END";
+            return 0;
+        }
+    }
 
     in.close();
     return 0;
