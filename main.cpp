08cc3b23dcf2b539110014935a79cf5817ea4c51
Dasha <grimalskayad@gmail.com>
2020-03-19 12:03:39 +0200

    Input maze, start, end nodes

--- main.cpp
+++ main.cpp
@@ -1,22 +1,36 @@
 #include <iostream>
 #include "Heap.h"
+#include <fstream>
+#include "Node.h"
+
+using namespace std;
+
+ifstream in;
+ofstream out;
 
 int main() {
-
-    Heap* heap = new Heap();
-
-    for (int i = 10; i >= 0; i--) {
-        heap->add(i);
+    in.open("../maze.txt");
+    int n, m;
+    in >> n >> m;
+    vector<vector<Node*> > grid(n, vector<Node*>(m, nullptr));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            char a;
+            in >> a;
+            Node* node = new Node(i, j, a == 'X');
+            grid[i][j] = node;
+        }
     }
+    int x, y;
+    cin >> x >> y;
+    Node* start = grid[x][y];
+    cin >> x >> y;
+    Node* end = grid[x][y];
 
-    heap->add(3);
-    heap->add(8);
-    heap->add(2);
 
-    while (!heap->isEmpty()) {
-        cout << heap->getMin() << endl;
-        heap->delMin();
-    }
 
+
+
+    in.close();
     return 0;
 }
