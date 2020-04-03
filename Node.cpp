b8043a1d8e4e3c6f14a5ff07efd88183c56d4361
Dasha <grimalskayad@gmail.com>
2020-04-03 13:17:55 +0300

    Knight's move

--- Node.cpp
+++ Node.cpp
@@ -13,8 +13,8 @@ Node::Node(int x, int y, bool isWall) {
 vector<Node *> Node::getNeighbours(const vector<vector<Node *> > &grid) {
     vector<Node*> neighbours;
 
-    if (x-1 >= 0)
-        neighbours.push_back(grid[x-1][y]);
+   /* if (x-1 >= 0)
+   neighbours.push_back(grid[x-1][y]);
 
     if (y-1 >= 0)
         neighbours.push_back(grid[x][y-1]);
@@ -24,6 +24,23 @@ vector<Node *> Node::getNeighbours(const vector<vector<Node *> > &grid) {
 
     if (y+1 < grid[x].size())
         neighbours.push_back(grid[x][y+1]);
+*/
+   if (x+2 < grid.size() && y+1 < grid[x].size())
+        neighbours.push_back(grid[x+2][y+1]);
+    if (x+2 < grid.size() && y-1 >= 0)
+        neighbours.push_back(grid[x+2][y-1]);
+    if (x+1 < grid.size() && y+2 < grid[x].size())
+        neighbours.push_back(grid[x+1][y+2]);
+    if (x+1 <= grid.size() && y-2 >= 0)
+        neighbours.push_back(grid[x+1][y-2]);
+    if (x-2  >= 0 && y+1 < grid[x].size())
+        neighbours.push_back(grid[x-2][y+1]);
+    if (x-2  >= 0 && y-1  >= 0)
+        neighbours.push_back(grid[x-2][y-1]);
+    if (x-1  >= 0 && y+2 < grid[x].size())
+        neighbours.push_back(grid[x-1][y+2]);
+    if (x-1 >= 0 && y-2  >= 0)
+        neighbours.push_back(grid[x-1][y-2]);
 
     return neighbours;
 }
